dfd828585914a8e1afd0e6fd0659ebbe65114a28
Sheng <demonicplayer404@gmail.com>
2025-08-15 15:04:25 -0400

    updated fith part

--- wiki.py
+++ wiki.py
@@ -1,3 +1,6 @@
 import wikipedia
 
 print(wikipedia.summary("Wikipedia"))
+print(wikipedia.search('Philosophy of life'))
+print(wikipedia.search("Python (programming language)"))
+print(wikipedia.search("Galaxy"))
